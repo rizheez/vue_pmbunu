d05efdfd96afbb14f1564ce6db705ac84c977fe5
rizheez <rezamuhammad980@gmail.com>
2026-04-20 14:16:02 +0800

    chore(changes): clean up empty code change sections

--- public/build/assets/Spinner.vue_vue_type_script_setup_true_lang-BkDh14-0.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-DFTzubab.js";import{L as r}from"./loader-circle-VMaGZbZ0.js";import{d as n,k as c,u as s,n as t,o as l}from"./app-D0tNvLvl.js";const f=n({__name:"Spinner",props:{class:{}},setup(a){const e=a;return(i,p)=>(l(),c(s(r),{role:"status","aria-label":"Loading",class:t(s(o)("size-4 animate-spin",e.class))},null,8,["class"]))}});export{f as _};
